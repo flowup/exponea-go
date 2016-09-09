bce16b77a0da3c17ceaff64e413d83bcc775500f
Gelidus <gelidus@flowup.eu>
2016-09-09 22:12:15 +0200

    refactor(editorconfig): editorconfig is now enabled on the project

--- exponea_base.go
+++ exponea_base.go
@@ -1,97 +1,97 @@
 package exponea
 
 import (
-  "net/http"
-  "encoding/json"
-  "bytes"
-  "io/ioutil"
+	"net/http"
+	"encoding/json"
+	"bytes"
+	"io/ioutil"
 )
 
 const (
-  DefaultAPIEndpoint = "https://api.exponea.com"
-  EventsEndpoint = "/crm/events"
-  CustomersEndpoint = "/crm/customers"
+	DefaultAPIEndpoint = "https://api.exponea.com"
+	EventsEndpoint = "/crm/events"
+	CustomersEndpoint = "/crm/customers"
 )
 
 // Event is an encapsulation for event sending request
 type Event struct {
-  Customers  map[string]string `json:"customer_ids"`
-  ProjectID  string `json:"project_id"`
-  Type       string `json:"type"`
-  Properties map[string]string `json:"properties"`
-  Timestamp  int `json:"timestamp,omitempty"`
+	Customers  map[string]string `json:"customer_ids"`
+	ProjectID  string `json:"project_id"`
+	Type       string `json:"type"`
+	Properties map[string]string `json:"properties"`
+	Timestamp  int `json:"timestamp,omitempty"`
 }
 
 // Customer is an encapsulation for customer value updating
 // request
 type Customer struct {
-  Customers  map[string]string `json:"ids"`
-  ProjectID  string `json:"project_id"`
-  Properties map[string]string `json:"properties"`
+	Customers  map[string]string `json:"ids"`
+	ProjectID  string `json:"project_id"`
+	Properties map[string]string `json:"properties"`
 }
 
 // Response defines errors and status of the call to the API
 type Response struct {
-  Errors  []string `json:"errors"`
-  Success bool `json:"success"`
+	Errors  []string `json:"errors"`
+	Success bool `json:"success"`
 }
 
 // Client implements single project binding to
 // exponea API
 type API struct {
-  // id of the project
-  projectID     string
+	// id of the project
+	projectID     string
 
-  projectSecret string
+	projectSecret string
 
-  // target API URL
-  target string
+	// target API URL
+	target        string
 
-  httpClient *http.Client
+	httpClient    *http.Client
 }
 
 // NewAPI creates new Client configuration based on the
 // given project ID
 func NewAPI(projectID, projectSecret string) *API {
-  return NewAPIWithTarget(projectID, projectSecret, DefaultAPIEndpoint)
+	return NewAPIWithTarget(projectID, projectSecret, DefaultAPIEndpoint)
 }
 
 // NewAPIWithTarget creates new API client and let's user configure
 // target endpoint. This can be used in case of testing or using more
 // Exponea api targets
 func NewAPIWithTarget(projectID, projectSecret, target string) *API {
-  return &API{
-    projectID: projectID,
-    projectSecret: projectSecret,
-    target: target,
-    httpClient: &http.Client{},
-  }
+	return &API{
+		projectID: projectID,
+		projectSecret: projectSecret,
+		target: target,
+		httpClient: &http.Client{},
+	}
 }
 
 // SendRequest sends request to the given endpoint, marshalling
 // the model to JSON and awaiting response of type Response
 func (c *API) SendRequest(url string, model interface{}) (*Response, error) {
-  requestData, err := json.Marshal(model)
-  if err != nil {
-    return nil, err
-  }
+	requestData, err := json.Marshal(model)
+	if err != nil {
+		return nil, err
+	}
 
-  resp, err := c.httpClient.Post(c.target + url, "application/json", bytes.NewReader(requestData))
-  if err != nil {
-    return nil, err
-  }
+	resp, err := c.httpClient.Post(c.target + url, "application/json", bytes.NewReader(requestData))
+	if err != nil {
+		return nil, err
+	}
 
-  responseData, err := ioutil.ReadAll(resp.Body)
-  if err != nil {
-    return nil, err
-  }
+	responseData, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
 
-  apiResponse := &Response{}
-  if err = json.Unmarshal(responseData, apiResponse); err != nil {
-    return nil, err
-  }
+	apiResponse := &Response{}
+	if err = json.Unmarshal(responseData, apiResponse); err != nil {
+		return nil, err
+	}
 
-  return apiResponse, nil
+	return apiResponse, nil
 }
 
 // SendEvent sends given event data to the Events endpoint
@@ -99,19 +99,19 @@ func (c *API) SendRequest(url string, model interface{}) (*Response, error) {
 // caused by the network or data serialization, an error will
 // be returned
 func (c *API) Track(event *Event) (*Response, error) {
-  if event.ProjectID == "" {
-    event.ProjectID = c.projectID
-  }
+	if event.ProjectID == "" {
+		event.ProjectID = c.projectID
+	}
 
-  return c.SendRequest(EventsEndpoint, event)
+	return c.SendRequest(EventsEndpoint, event)
 }
 
 // SendCustomer sends given event data to the Customers endpoint
 // @see SendEvent
 func (c *API) Update(customer *Customer) (*Response, error) {
-  if customer.ProjectID == "" {
-    customer.ProjectID = c.projectID
-  }
+	if customer.ProjectID == "" {
+		customer.ProjectID = c.projectID
+	}
 
-  return c.SendRequest(CustomersEndpoint, customer)
+	return c.SendRequest(CustomersEndpoint, customer)
 }
--- exponea_base_test.go
+++ exponea_base_test.go
@@ -1,74 +1,74 @@
 package exponea
 
 import (
-  "github.com/stretchr/testify/suite"
-  "testing"
-  "github.com/stretchr/testify/assert"
-  "net/http/httptest"
-  "net/http"
-  "encoding/json"
+	"github.com/stretchr/testify/suite"
+	"testing"
+	"github.com/stretchr/testify/assert"
+	"net/http/httptest"
+	"net/http"
+	"encoding/json"
 )
 
 const (
-  mockClientProjectID = "id"
-  mockClientProjectSecret = "secret"
+	mockClientProjectID = "id"
+	mockClientProjectSecret = "secret"
 )
 
 type APISuite struct {
-  suite.Suite
+	suite.Suite
 
-  server *httptest.Server
-  api    *API
+	server *httptest.Server
+	api    *API
 }
 
 func (s *APISuite) SetupSuite() {
-  // create mock server
-  s.server = httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-    successData, _ := json.Marshal(&Response{})
-    w.Write(successData)
-  }))
+	// create mock server
+	s.server = httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		successData, _ := json.Marshal(&Response{})
+		w.Write(successData)
+	}))
 
-  // create new API
-  s.api = NewAPIWithTarget(mockClientProjectID, mockClientProjectSecret, s.server.URL)
-  assert.NotEqual(s.T(), nil, s.api)
-  assert.Equal(s.T(), mockClientProjectID, s.api.projectID)
-  assert.Equal(s.T(), mockClientProjectSecret, s.api.projectSecret)
+	// create new API
+	s.api = NewAPIWithTarget(mockClientProjectID, mockClientProjectSecret, s.server.URL)
+	assert.NotEqual(s.T(), nil, s.api)
+	assert.Equal(s.T(), mockClientProjectID, s.api.projectID)
+	assert.Equal(s.T(), mockClientProjectSecret, s.api.projectSecret)
 }
 
 func (s *APISuite) TearDownSuite() {
-  // close the server listener on cleanup
-  s.server.Close()
+	// close the server listener on cleanup
+	s.server.Close()
 }
 
 func (s *APISuite) TestTrack() {
-  resp, err := s.api.Track(&Event{
-    Customers: map[string]string{
-      "registered": "peter.malina@flowup.eu",
-    },
-    Type: "registration",
-    Properties: map[string]string{
-      "property": "and it's valueee",
-    },
-  })
+	resp, err := s.api.Track(&Event{
+		Customers: map[string]string{
+			"registered": "peter.malina@flowup.eu",
+		},
+		Type: "registration",
+		Properties: map[string]string{
+			"property": "and it's valueee",
+		},
+	})
 
-  assert.Equal(s.T(), nil, err)
-  assert.NotEqual(s.T(), nil, resp)
+	assert.Equal(s.T(), nil, err)
+	assert.NotEqual(s.T(), nil, resp)
 }
 
 func (s *APISuite) TestUpdate() {
-  resp, err := s.api.Update(&Customer{
-    Customers: map[string]string{
-      "registered": "peter.malina@flowup.eu",
-    },
-    Properties: map[string]string{
-      "property": "and it's valueee",
-    },
-  })
+	resp, err := s.api.Update(&Customer{
+		Customers: map[string]string{
+			"registered": "peter.malina@flowup.eu",
+		},
+		Properties: map[string]string{
+			"property": "and it's valueee",
+		},
+	})
 
-  assert.Equal(s.T(), nil, err)
-  assert.NotEqual(s.T(), nil, resp)
+	assert.Equal(s.T(), nil, err)
+	assert.NotEqual(s.T(), nil, resp)
 }
 
 func TestAPISuite(t *testing.T) {
-  suite.Run(t, &APISuite{})
+	suite.Run(t, &APISuite{})
 }
